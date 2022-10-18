1f8a1407c0914ee10b6101500814ac23b33fe858
Julian Gomez <julian1995ag@gmail.com>
2022-10-18 12:02:04 -0300

    clase 7

---
+++ app.js
@@ -0,0 +1,24 @@
+require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+
+const main = async()=>{
+
+    console.log('hola mundo')
+    let opt = ''
+   do{
+        opt = await mostrarMenu();
+        console.log({opt})
+
+        if(opt !== '0') {
+            await pausa()
+        }
+        
+   }while(opt!=='0');
+
+   
+    
+
+}
+
+main()
